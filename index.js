6674f3e94f9e29463001bf78808163f41cb65e58
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-06-15 23:10:56 +0530

    Update index.js
    adding frontend deployed url link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.options('*',cors());
 var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://webscrape-ecommerce-frontend.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();  
